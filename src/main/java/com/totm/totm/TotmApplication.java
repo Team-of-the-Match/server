fb97cb472c8ddce71975640d8c74f2c242b3fb70
ChangHyun Yoo <jakey1110@naver.com>
2024-01-26 21:49:19 +0900

    initial commit

---
+++ src/main/java/com/totm/totm/TotmApplication.java
@@ -0,0 +1,13 @@
+package com.totm.totm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totm/totm/TotmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totm.totm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
