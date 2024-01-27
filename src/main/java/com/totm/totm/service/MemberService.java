3c11f55b5c3fa58d166291a505b23e9816ce2bdd
ChangHyun Yoo <jakey1110@naver.com>
2024-01-27 20:26:38 +0900

    branch for 'develop'

---
+++ src/main/java/com/totm/totm/service/MemberService.java
@@ -0,0 +1,16 @@
+package com.totm.totm.service;
+
+import com.totm.totm.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+}
